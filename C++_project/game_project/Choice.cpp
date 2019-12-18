531d564896017ff4b4637957c54abee732178fc5
yhujinhui <52348335+yhujinhui@users.noreply.github.com>
2019-12-18 16:18:43 +0900

    c++ 최종

--- C++_project/game_project/Choice.cpp
+++ C++_project/game_project/Choice.cpp
@@ -139,18 +139,18 @@ void title_change() {
 		title();
 		int title_menucode = title_menuchoice();
 		if (title_menucode == 0) {//게임 시작
-			//gp->game_screen();
-			gp->ending1();
+			gp->game_screen();
+			//gp->ending1();
 		}
 		if (title_menucode == 2) {//게임 방법
-			//gamerule();
-			gp->ending2();
+			gamerule();
+			//gp->ending2();
 
 		}
 		if (title_menucode == 4) {//스토리 보기
-			//system("cls");
-			//intro1();
-			gp->ending3();
+			system("cls");
+			intro1();
+			//gp->ending3();
 
 		}
 		if (title_menucode == 6) {//랭킹보기
